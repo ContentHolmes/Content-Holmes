2dc6a91184a8dcbdeee15fd98a68d81309ed5905
Anirudh Krishna <f2015081@pilani.bits-pilani.ac.in>
2017-01-29 13:20:59 +0530

    Add files via upload

--- js/img_check.js
+++ js/img_check.js
@@ -4,18 +4,18 @@ function imagecheck(imageurl, image){
     "detectOrientation" : "true",
   };
 
-  var url = { "url" : "http://img.tfd.com/cde/_LSYJPG2.JPG"};
-  console.log(String(url));
+  var url = { url : "http://img.tfd.com/cde/_LSYJPG2.JPG" };
+  console.log(JSON.stringify(url) );
   console.log(params);
 
   $.ajax({
-    url : String("https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr?" + $.param(params)),
+    url: "https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr?" + $.param(params),
     beforeSend: function(xhrObj){
       xhrObj.setRequestHeader("Content-Type","application/json");
       xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "82535cdc5d3849cea09575b48b3b1e98");
     },
-    type : "POST",
-    data : url,
+    data: JSON.stringify(url),
+    type: "POST",
   })
   .done(function(data){
         console.log("success");
@@ -27,7 +27,14 @@ function imagecheck(imageurl, image){
     console.log("fail");
   });
 
+  var temp = document.getElementById(image);
+  var width = temp.style.width;
+  var height = temp.style.height;
+  temp.style.width = 400;
+  temp.style.height = 400;
   nudecheck(image);
+  temp.style.width = width;
+  temp.style.height = height;
 }
 
 function nudecheck(image){
@@ -51,7 +58,7 @@ function testnude(){
   image1.width = "400";
   image1.height = "400";
   image1.id = "test4";
-  image1.style.visibility = "hidden"
+  image1.style.visibility = "hidden";
   image1.src = "./../js/images/4.jpg";
   console.log(image1.id);
   nudecheck(image1.id);
@@ -70,5 +77,5 @@ function testimage(){
   imagecheck(image1.src, image1.id);
 }
 
-testimage();
+// testimage();
 // testnude();
