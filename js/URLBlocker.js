a69f11d30c0aedf36ee203081252d3a01bedb4e8
Mrigesh Madaan <f2015756@pilani.bits-pilani.ac.in>
2017-01-30 18:34:33 +0530

    for rajat

--- js/URLBlocker.js
+++ js/URLBlocker.js
@@ -248,3 +248,666 @@ var globalBadCount=0;
 var globalGoodCount=0;
 var qualifiedImagesArray=[];
 
+words = [
+"anus",
+"arse",
+"arsehole",
+"ass",
+"ass-hat",
+"ass-jabber",
+"ass-pirate",
+"assbag",
+"assbandit",
+"assbanger",
+"assbite",
+"assclown",
+"asscock",
+"asscracker",
+"asses",
+"assface",
+"assfuck",
+"assfucker",
+"assgoblin",
+"asshat",
+"asshead",
+"asshole",
+"asshopper",
+"assjacker",
+"asslick",
+"asslicker",
+"assmonkey",
+"assmunch",
+"assmuncher",
+"assnigger",
+"asspirate",
+"assshit",
+"assshole",
+"asssucker",
+"asswad",
+"asswipe",
+"axwound",
+"bampot",
+"bastard",
+"beaner",
+"bitch",
+"bitchass",
+"bitches",
+"bitchtits",
+"bitchy",
+"blow job",
+"blowjob",
+"bollocks",
+"bollox",
+"boner",
+"brotherfucker",
+"bullshit",
+"bumblefuck",
+"butt plug",
+"butt-pirate",
+"buttfucka",
+"buttfucker",
+"camel toe",
+"carpetmuncher",
+"chesticle",
+"chinc",
+"chink",
+"choad",
+"chode",
+"clit",
+"clitface",
+"clitfuck",
+"clusterfuck",
+"cock",
+"cockass",
+"cockbite",
+"cockburger",
+"cockface",
+"cockfucker",
+"cockhead",
+"cockjockey",
+"cockknoker",
+"cockmaster",
+"cockmongler",
+"cockmongruel",
+"cockmonkey",
+"cockmuncher",
+"cocknose",
+"cocknugget",
+"cockshit",
+"cocksmith",
+"cocksmoke",
+"cocksmoker",
+"cocksniffer",
+"cocksucker",
+"cockwaffle",
+"coochie",
+"coochy",
+"coon",
+"cooter",
+"cracker",
+"cum",
+"cumbubble",
+"cumdumpster",
+"cumguzzler",
+"cumjockey",
+"cumslut",
+"cumtart",
+"cunnie",
+"cunnilingus",
+"cunt",
+"cuntass",
+"cuntface",
+"cunthole",
+"cuntlicker",
+"cuntrag",
+"cuntslut",
+"dago",
+"damn",
+"deggo",
+"dick",
+"dick-sneeze",
+"dickbag",
+"dickbeaters",
+"dickface",
+"dickfuck",
+"dickfucker",
+"dickhead",
+"dickhole",
+"dickjuice",
+"dickmilk",
+"dickmonger",
+"dicks",
+"dickslap",
+"dicksucker",
+"dicksucking",
+"dicktickler",
+"dickwad",
+"dickweasel",
+"dickweed",
+"dickwod",
+"dike",
+"dildo",
+"dipshit",
+"doochbag",
+"dookie",
+"douche",
+"douche-fag",
+"douchebag",
+"douchewaffle",
+"dumass",
+"dumb ass",
+"dumbass",
+"dumbfuck",
+"dumbshit",
+"dumshit",
+"dyke",
+"fag",
+"fagbag",
+"fagfucker",
+"faggit",
+"faggot",
+"faggotcock",
+"fagtard",
+"fatass",
+"fellatio",
+"feltch",
+"flamer",
+"fuck",
+"fuckass",
+"fuckbag",
+"fuckboy",
+"fuckbrain",
+"fuckbutt",
+"fuckbutter",
+"fucked",
+"fucker",
+"fuckersucker",
+"fuckface",
+"fuckhead",
+"fuckhole",
+"fuckin",
+"fucking",
+"fucknut",
+"fucknutt",
+"fuckoff",
+"fucks",
+"fuckstick",
+"fucktard",
+"fucktart",
+"fuckup",
+"fuckwad",
+"fuckwit",
+"fuckwitt",
+"fudgepacker",
+"gay",
+"gayass",
+"gaybob",
+"gaydo",
+"gayfuck",
+"gayfuckist",
+"gaylord",
+"gaytard",
+"gaywad",
+"goddamn",
+"goddamnit",
+"gooch",
+"gook",
+"gringo",
+"guido",
+"handjob",
+"hard on",
+"heeb",
+"hell",
+"ho",
+"hoe",
+"homo",
+"homodumbshit",
+"honkey",
+"humping",
+"jackass",
+"jagoff",
+"jap",
+"jerk off",
+"jerkass",
+"jigaboo",
+"jizz",
+"jungle bunny",
+"junglebunny",
+"kike",
+"kooch",
+"kootch",
+"kraut",
+"kunt",
+"kyke",
+"lameass",
+"lardass",
+"lesbian",
+"lesbo",
+"lezzie",
+"mcfagget",
+"mick",
+"minge",
+"mothafucka",
+"mothafuckin'",
+"motherfucker",
+"motherfucking",
+"muff",
+"muffdiver",
+"munging",
+"nude",
+"negro",
+"nigaboo",
+"nigga",
+"nigger",
+"niggers",
+"niglet",
+"nut sack",
+"nutsack",
+"porn",
+"paki",
+"panooch",
+"pecker",
+"peckerhead",
+"penis",
+"penisbanger",
+"penisfucker",
+"penispuffer",
+"piss",
+"pissed",
+"pissed off",
+"pissflaps",
+"polesmoker",
+"pollock",
+"poon",
+"poonani",
+"poonany",
+"poontang",
+"porch monkey",
+"porchmonkey",
+"prick",
+"punanny",
+"punta",
+"pussies",
+"pussy",
+"pussylicking",
+"puto",
+"queef",
+"queer",
+"queerbait",
+"queerhole",
+"renob",
+"rimjob",
+"ruski",
+"sand nigger",
+"sandnigger",
+"schlong",
+"scrote",
+"shit",
+"shitass",
+"shitbag",
+"shitbagger",
+"shitbrains",
+"shitbreath",
+"shitcanned",
+"shitcunt",
+"shitdick",
+"shitface",
+"shitfaced",
+"shithead",
+"shithole",
+"shithouse",
+"shitspitter",
+"shitstain",
+"shitter",
+"shittiest",
+"shitting",
+"shitty",
+"shiz",
+"shiznit",
+"skank",
+"skeet",
+"skullfuck",
+"slut",
+"slutbag",
+"smeg",
+"spic",
+"spick",
+"splooge",
+"spook",
+"suckass",
+"tard",
+"testicle",
+"thundercunt",
+"tit",
+"titfuck",
+"tits",
+"tittyfuck",
+"twat",
+"twatlips",
+"twats",
+"twatwaffle",
+"unclefucker",
+"va-j-j",
+"vag",
+"vagina",
+"vajayjay",
+"vjayjay",
+"wank",
+"wankjob",
+"wetback",
+"whore",
+"whorebag",
+"whoreface",
+"wop",
+]
+
+
+
+
+var blockedit=false;
+console.log("value of blocked it at initialization is: "+blockedit);
+
+function BlockURL(){
+  var urlString=getName(document.location.href);
+<<<<<<< HEAD
+  console.log("message #:"+ urlString);
+  
+  chrome.storage.local.get(['settings', 'global'], function(items){
+    for(var i in bannedElementsArray){
+      if(bannedElementsArray[i]==urlString){
+        blockedit=true;
+        console.log("value of blocked it is: "+blockedit);
+        var arr=document.getElementsByTagName('body');
+        for(var i in arr){
+          arr[i].innerHTML="Not permitted to view this";  
+        }
+        if(items.global.historyOfBlockedURLS.length<5){
+          var obj={"time":new Date(),"URL":document.location.href};
+          items.global.historyOfBlockedURLS.push(obj);
+          chrome.storage.local.set({ global: items.global });
+          //console.log(items.global);
+          console.log("From BLOCK URL 1:"+items.global.historyOfBlockedURLS);
+        }
+        else{
+          items.global.historyOfBlockedURLS.shift();
+          var obj={"time":new Date(),"URL":document.location.href};
+          items.global.historyOfBlockedURLS.push(obj);
+          chrome.storage.local.set({ global: items.global });
+          //console.log(items.global);
+          console.log("From BLOCK URL 2"+items.global.historyOfBlockedURLS);
+          // so how do we send data to server
+        } 
+        break;
+=======
+  console.log(urlString);
+  for(var i in bannedElementsArray){
+    if(bannedElementsArray[i]==urlString){
+      var arr=document.getElementsByTagName('body');
+      for(var i in arr){
+      	// $.post(/*URL Here*/,{
+      	// 		"Type": "URL",
+      	// 		"Data": document.location.href,
+      	// 		"User-ID": /*userIDs here*/
+      	// 	},function(data, status) {
+      	// 	console.log("Data sent");
+      	// });
+		chrome.runtime.sendMessage({redirect: chrome.extension.getURL("/html/safetypage.html")});
+>>>>>>> b907f1a250396086088b6a618b7de1c095f334e2
+      }
+    }
+  });
+  chrome.storage.local.get(['settings','global'],function(items){
+    for(var u in items.global.bannedURLs){
+      if(items.global.bannedURLs[u]==urlString){
+        blockedit=true;
+        console.log("value of blocked it is: "+blockedit);
+        var arr=document.getElementsByTagName('body');
+        for(var i in arr){
+          arr[i].innerHTML="Not permitted to view this";  
+        }
+        if(items.global.historyOfBlockedURLS.length<5){
+            var obj={"time":new Date(),"URL":document.location.href};
+            items.global.historyOfBlockedURLS.push(obj);
+            chrome.storage.local.set({ global: items.global });
+            //console.log(items.global);
+            console.log("From BLOCK URL 1:"+items.global.historyOfBlockedURLS);
+          }
+        else{
+            items.global.historyOfBlockedURLS.shift();
+            var obj={"time":new Date(),"URL":document.location.href};
+            items.global.historyOfBlockedURLS.push(obj);
+            chrome.storage.local.set({ global: items.global });
+            //console.log(items.global);
+            console.log("From BLOCK URL 2"+items.global.historyOfBlockedURLS);
+            // so how do we send data to server
+          } 
+        break;
+      }
+    }
+  });
+  if(blockedit==false){
+    console.log("This log you need to look for");
+    console.log("value of blocked it is: "+blockedit);
+    checkNudeImages();
+    console.log("This is the second message you need to look for");
+  }
+}
+
+function getName(str) {
+  const regex = /\/\/w{0,3}\.?(.*)\.\w{1,4}\/.*/gi;
+  str = str +"/";
+  var name = regex.exec(str);
+  //console.log(name[1]);
+  return name[1];
+}
+
+function getImageName(str) {
+  const regex = /\/\/.{1,3}\.([^\/]*)\.\w{1,4}\//g;
+  var name = regex.exec(str);
+  return name[1];
+}
+
+<<<<<<< HEAD
+BlockURL();
+
+//###################################################################################################################
+
+
+
+// first check the URL of the image. 
+//  first check if the safe search if true of not
+
+
+
+function checkNudeImages(){
+  for(var k in imagesArray){
+
+    try{var url=getImageName(imagesArray[k].src);}
+    catch(err){console.log(err);}
+    console.log("URL is:" + url);
+    if(checkPresenceInBanned(url)){
+      imagesArray[k].style.visibility="hidden";
+      console.log("Hid the image: "+imagesArray[k].src);
+    }
+    else if(!checkPresenceInTrusted(url)){
+      if((imagesArray[k].clientWidth>300 || imagesArray[k].clientHeight>300) && checkCount<=10){
+        console.log("IMAGE MUST BE CHECKED:\n"+imagesArray[k].src);
+        NudeCheck(imagesArray[k]);
+        checkCount++;
+      }    
+    }
+  }
+}
+
+function NudeCheck(image){
+  console.log("yo bro:"+image.src);
+  console.log("The URL from Nude CHeck and get name:\n"+getName(image.src));
+  var params = {
+  // Request parameters
+  "visualFeatures": "Adult",
+  "language": "en",
+  };
+  var url = { url : image.src };
+  console.log("From Nude Check:\n"+JSON.stringify(url));
+  $.ajax({
+  url: "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?" + $.param(params),
+  beforeSend: function(xhrObj){
+    // Request headers
+    xhrObj.setRequestHeader("Content-Type","application/json");
+    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","82535cdc5d3849cea09575b48b3b1e98");
+  },
+  
+  type: "POST",
+  data: JSON.stringify(url),
+  // Request body
+  })
+  .done(function(data){validateNudeResults(data,image);})
+  .fail(function() {
+    console.log("error");
+  });
+}
+function validateNudeResults(data,image){
+  // if bad -> blur using index increase badcount
+  // if good ->increase good count
+  // if globalBadCout==5, put it in local sotage usin getName
+  // if put in local storage trusted  using getName
+  console.log(JSON.stringify(data));
+  console.log("image src from validate Nudes:"+image.src);
+  if(data.adult.isAdultContent){
+    globalBadCount++;
+    console.log("hiding image: "+image.src);
+    try{
+      image.style.visibility="hidden";
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+  else{
+    globalGoodCount++;
+    try{
+      console.log("visiblising image: "+ image.src);
+      image.style.visibility="visible";
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+  if(globalBadCount==3){
+    var bodyArr=document.getElementsByTagName("body");
+    for(var x in bodyArr){
+      bodyArr[x].innerHTML="Not permitted to view this";
+    }     
+    chrome.storage.local.get(['settings', 'global'], function(items){
+      items.global.bannedURLs.push(getImageName(image.src));
+      chrome.storage.local.set({ global: items.global });
+      console.log("added a new URL to blocked sites: "+image.src);
+      console.log(JSON.stringify(items.global.bannedURLs));
+    });
+  }
+  else if(globalGoodCount==10){
+    chrome.storage.local.get(['settings', 'global'], function(items){
+      items.global.trustedURLs.push(getImageName(image.src));
+      chrome.storage.local.set({ global: items.global });
+      console.log("added a new URL in the trusted sites list: "+images.src);
+      console.log(JSON.stringify(items.global.trustedURLs));
+    });
+  }
+
+}
+
+function checkPresenceInBanned(url){
+  // take things from chrome local storage
+  var str=url;
+  var bad=false;
+  for(var i in bannedElementsArray){
+    if(bannedElementsArray[i]==str){
+      bad=true;
+      break;
+    }
+  }
+  chrome.storage.local.get(['settings', 'global'], function(items){
+    var localBannedArray=items.global.bannedURLs;
+    for(var j in localBannedArray){
+      console.log("inside step 1");
+      if(str==localBannedArray[j]){
+        bad=true;
+        console.log("inside step 2");
+        break;
+      }
+    }
+  });
+  console.log("present in bad: "+bad);
+  return bad;
+}
+function checkPresenceInTrusted(url){
+  // take things from chrome local storage
+  var str=url;
+  var good=false;
+  for(var i in trustedElementsArray){
+    if(trustedElementsArray[i]==str){
+      good=true;
+      break;
+    }
+  }
+  chrome.storage.local.get(['settings', 'global'], function(items){
+    var localTrustedArray=items.global.trustedURLs;
+    for(var i in localTrustedArray){
+      if(localTrustedArray[i]==str){
+        good=true;
+        break;
+      }
+    }
+  });
+  console.log("present in good: "+good);
+  return good; 
+}
+=======
+function urlcheck(url) {
+	var params = getUrlVars(url);
+	if(params.q != null) {
+		return paramscheck(params.q);
+	} else if(params.search_query!=null){
+		return paramscheck(params.search_query);
+	} else {
+		return 0;
+	}
+}
+
+function paramscheck(params) {
+	count = 0.0;
+	bad = 0.0;
+	if(params.includes("+")) {
+		params = params.split("+");
+	} else {
+		params = params.split("%");
+	}
+	count = params.length;
+	for(i=0;i< words.length;i++) {
+		if(params.indexOf(words[i])!=-1) {
+			bad++;
+		}
+	}
+	return bad/count;
+}
+
+function getUrlVars(href)
+{
+    var vars = [], hash;
+    var hashes = href.slice(href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+if(urlcheck(document.location.href)<=0.1) {
+	BlockURL();
+} else {
+	console.log(chrome.extension.getURL("/html/safetypage"));
+	chrome.runtime.sendMessage({redirect: chrome.extension.getURL("/html/safetypage.html")});
+}
+>>>>>>> b907f1a250396086088b6a618b7de1c095f334e2
--- js/event.js
+++ js/event.js
@@ -1,2 +1,58 @@
 var currentVersion = '2.0.10';
 
+chrome.storage.local.get(['settings', 'global'], function(items) {
+	var global = items.global || { };
+
+	// Update icon
+	// if( global.enabled ) {
+	// 	chrome.browserAction.setIcon( { path: 'images/icon_19.png' } );
+	// }
+	// else {
+	// 	chrome.browserAction.setIcon( { path: 'images/icon_19_disabled.png' } );
+	// }
+
+	if( global.version != currentVersion ) {
+		// Update version
+		global.version = currentVersion;
+		// Update some variables to their default value
+		global.enabled = true;
+		// set it as object {"date":"","URL":""}
+		global.showChangelog = true;
+	}
+
+	global.newVar="fdafda";
+	global.historyOfBlockedURLS=[];
+	global.bannedURLs=[];
+	global.trustedURLs=[];
+	chrome.storage.local.set({ global: global });
+});
+
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+  console.log("something must happen right now");
+  if(request.message == "sentiment"){
+    console.log("sentiment will be fired");
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {message: "FireSentiment"}, function(response) {
+	    console.log(response.message);
+	  });
+	});
+	console.log("this function ran");
+    sendResponse({message:"good luck bro"});
+  } else /*if(request.message == "redirect")*/ {
+  		console.log("I am here");
+    	chrome.tabs.update(sender.tab.id, {url: request.redirect});
+    	sendResponse({message:"good luck bro2"});
+  }
+  
+});
+<<<<<<< HEAD
+=======
+
+// chrome.tabs.query({"active":true},function(tab){
+// 	console.log(tab[0].url.toString());
+// });
+// chrome.tabs.getCurrent(function(tab){
+//         console.log(tab.url);
+//     }
+// );
+>>>>>>> b907f1a250396086088b6a618b7de1c095f334e2
