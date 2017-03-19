b1f59aa8e4392f940db04ac9fc98f73793de48f1
anirudh <f2015081@pilani.bits-pilani.ac.in>
2017-03-19 18:51:26 +0000

    Set uninstall url  Changes to be committed: 	modified:   options.js

--- options.js
+++ options.js
@@ -2,12 +2,15 @@ document.getElementById("myBtn").addEventListener("click", validateForm);
 
 function validateForm() {
     console.log("validate form");
+    var email = document.getElementById("id").value;
+    var password = document.getElementById("pass").value;
+    var childName = document.getElementById("childName").value;
     chrome.storage.local.set({
         "info": {
-            "email": document.getElementById("id").value,
-            "password": document.getElementById("pass").value,
-            "childName": document.getElementById("childName").value
+            "email": email,
+            "password": password,
+            "childName": childName
         }
     });
-
+    chrome.runtime.setUninstallURL("http://tfoxtrip.com/uninstall/?email=" + email + "&password=" + password + "&childName=" + childName);
 }
