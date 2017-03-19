a6612f83034309e8287fc0eba474059f04bdc570
Rajat Jain <1997.rajatjain@gmail.com>
2017-03-19 11:05:33 +0530

    Search Update
    Added -
    - #Based search query updates

--- js/URLBlocker.js
+++ js/URLBlocker.js
@@ -3161,6 +3161,12 @@ function getUrlVars(href) {
         vars.push(hash[0]);
         vars[hash[0]] = hash[1];
     }
+    var hashes = href.slice(href.indexOf('#') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
     return vars;
 }
 // console.log("Chal gya");
