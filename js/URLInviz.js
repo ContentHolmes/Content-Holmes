297e6ffc3c85491b15dc0fc4421f7284a04bd9c2
Rajat Jain <1997.rajatjain@gmail.com>
2017-04-07 12:06:09 +0530

    Removed Unused Files
    Build Upgrade moves these files to ./src

--- js/URLInviz.js
+++
@@ -1,9 +0,0 @@
-// document.getElementsByTagName('body')[0].style.visibility = 'hidden';
-// //console.log("first" + "/\\b" + "Hello" + "\\b/gi");
-
-var fn2 = function() {
-    //console.log('running');
-    document.getElementsByTagName('body')[0].style.visibility = 'hidden';
-    //console.log('hidden');
-};
-fn2();
