ead8763ae89d9e3bf6236e9a30b4d1974263a729
Rajat Jain <1997.rajatjain@gmail.com>
2017-05-27 16:31:02 +0530

    LFUCache implementation of list managers init
    Implemented LFUCache in both the managers. Server calls and hashtables on the server remain. Promise are needed to be implemented soon as the current code is broken.

--- src/js/modules/urlblock/bannedmanager.js
+++ src/js/modules/urlblock/bannedmanager.js
@@ -1,4 +1,6 @@
 var data = require('../data/URLBlocker.js');
+var md5 = require('md5');
+var lfu = require('lfu-cache')(1000, 3600*24*10);
 
 export {
     checkPresenceInBanned, add
@@ -10,38 +12,58 @@ function checkPresenceInBanned(url) {
     // checks the presence of the URL in the
     // 1. bannedElementsArray
     // 2. bannedURLs (from chrome local storage)
-    var str = url;
-    var bad = false;
+    var str;
+    if(url) {
+        str = url;
+    } else {
+        return false;
+    }
     for (var i in bannedElementsArray) {
         if (bannedElementsArray[i] == str) {
-            bad = true;
-            return bad;
+            return true;
             break;
         }
     }
     chrome.storage.local.get(['settings', 'global'], function(items) {
-        var localBannedArray = items.global.bannedURLs;
-        for (var j in localBannedArray) {
-            // //console.log("inside step 1");
-            // //console.log(localBannedArray[j]);
-            if (str == localBannedArray[j]) {
-                bad = true;
-                ////console.log("inside step 2");
-                return bad;
-                break;
-            }
+        var localBannedCache = items.global.bannedURLs;
+        lfu.import(localBannedCache);
+        if(getLFUCache(str)) {
+            return true;
         }
-        return bad;
+        if(items.global.bannedURLObj[md5(str)]) {
+            return true;
+        }
+        return false;
     });
+    //Server call goes here
 }
 
 function add(url) {
+    setLFUCache(url);
     chrome.storage.local.get(['settings', 'global'], function(items) {
-            items.global.bannedURLs.push(url);
-            chrome.storage.local.set({
-                global: items.global
-            });
-            console.log("added a new URL to blocked sites: " + url);
-            ////console.log(JSON.stringify(items.global.bannedURLs));
+        items.global.bannedURLs = lfu.export();
+        chrome.storage.local.set({
+            global: items.global
         });
+        console.log("added a new URL to blocked sites: " + url);
+    });
+    //Server call goes here
 }
+
+function setLFUCache(url) {
+    lfu.set(md5(url),{url: url});
+}
+
+function getLFUCache(url) {
+    return lfu.get(md5(url));
+}
+
+lfu.on('eviction', function(key, object) {
+    chrome.storage.local.get(['settings', 'global'], function(items) {
+        items.global.bannedURLObj[key] = object["url"];
+        chrome.storage.local.set({
+            global: items.global
+        });
+        console.log("added a new URL to blocked sites: " + url);
+    });
+});
--- src/js/modules/urlblock/trustedmanager.js
+++ src/js/modules/urlblock/trustedmanager.js
@@ -1,4 +1,6 @@
 var data = require('../data/URLBlocker.js');
+var md5 = require('md5');
+var lfu = require('lfu-cache')(1000, 3600*24*10);
 
 export {
     checkPresenceInTrusted, add
@@ -10,35 +12,57 @@ function checkPresenceInTrusted(url) {
     // checks the presence of url in
     // 1. trustedElementsArray
     // 2. trustedURLs (from chrome local storage)
-    var str = url;
-    var good = false;
+    var str;
+    if(url) {
+        str = url;
+    } else {
+        return false;
+    }
     for (var i in trustedElementsArray) {
         if (trustedElementsArray[i] == str) {
-            good = true;
-            return good;
+            return true;
             break;
         }
     }
     chrome.storage.local.get(['settings', 'global'], function(items) {
-        var localTrustedArray = items.global.trustedURLs;
-        for (var i in localTrustedArray) {
-            if (localTrustedArray[i] == str) {
-                good = true;
-                return good;
-                break;
-            }
+        var localTrustedCache = items.global.trustedURLs;
+        lfu.import(localTrustedCache);
+        if(getLFUCache(str)) {
+            return true;
         }
-        return good;
+        if(items.global.trustedURLObj[md5(str)]) {
+            return true;
+        }
+        return false;
     });
 }
 
 function add(url) {
+    setLFUCache(url);
     chrome.storage.local.get(['settings', 'global'], function(items) {
-        items.global.trustedURLs.push(getImageName(url));
+        items.global.trustedURLs = lfu.export();
         chrome.storage.local.set({
             global: items.global
         });
-        console.log("added a new URL in the trusted sites list: ");
-        //console.log(JSON.stringify(items.global.trustedURLs));
+        console.log("added a new URL to trusted sites: " + url);
     });
+    //Server call goes here
 }
+
+function setLFUCache(url) {
+    lfu.set(md5(url),{url: url});
+}
+
+function getLFUCache(url) {
+    return lfu.get(md5(url));
+}
+
+lfu.on('eviction', function(key, object) {
+    chrome.storage.local.get(['settings', 'global'], function(items) {
+        items.global.trustedURLObj[key] = object["url"];
+        chrome.storage.local.set({
+            global: items.global
+        });
+        console.log("added a new URL to trusted sites: " + url);
+    });
+});
