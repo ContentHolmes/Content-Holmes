0320f7d614c3518d1442ba132b3f8fa15dba2341
Rajat Jain <1997.rajatjain@gmail.com>
2017-08-27 16:38:50 +0530

    Added Docs
    Updated:
     - Tutorials
     - Documentation on the first page

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
                 destination: 'docs',
                 configure: 'jsdoc.json',
                 template: './node_modules/ink-docstrap/template',
+                tutorials: './src/js/',
                 recurse: true
             }
         }
